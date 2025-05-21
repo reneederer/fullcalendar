6121d6bd7de356ce863ae94c409da7db5279cbfb
reneederer <rene.ederer@protonmail.com>
2025-05-21 20:00:18 +0200

    -

--- src/Client/FullCalendarBindings.fs
+++ src/Client/FullCalendarBindings.fs
@@ -3,6 +3,7 @@ module FullCalendarBindings
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.React
+open Fable.Core.JS
 
 [<ImportDefault("@fullcalendar/react")>]
 let FullCalendarComponent: obj = jsNative
@@ -25,7 +26,55 @@ let germanLocale: obj = jsNative
 
 
 type CalendarEvent = {
-    title: string
-    start: string
-    ``end``: string
+    Id : int
+    Title: string
+    Start: string
+    End: string
 }
+
+type CalendarSetup =
+    { Events : CalendarEvent list
+      CreateEventF : obj -> unit
+      MoveEventF : obj -> unit
+      DeleteEventF : obj -> unit
+      EditEventF : obj -> unit
+    }
+
+
+let calendarComponent (setup : CalendarSetup) =
+    let calendarEvents =
+        setup.Events
+        |> List.map (fun event ->
+            createObj [
+                "title" ==> event.Title
+                "start" ==> event.Start
+                "end" ==> event.End
+                "id" ==> event.Id
+            ])
+        |> List.toArray
+
+    let calendarProps =
+        createObj [
+            "plugins" ==> [| dayGridPlugin; timeGridPlugin; interactionPlugin |]
+            "initialView" ==> "timeGridWeek"
+            "editable" ==> true
+            "droppable" ==> true
+            "events" ==> calendarEvents
+
+            "eventDrop" ==> (fun (info: obj) ->
+                console.log("Event moved:", info)
+                setup.MoveEventF info |> ignore
+            )
+
+            "drop" ==> (fun (info: obj) ->
+                console.log("External element dropped:", info)
+                setup.CreateEventF info |> ignore
+            )
+            "locale" ==> "de"
+            "locales" ==> [| germanLocale |]
+            "slotMinTime" ==> "06:00"
+            "slotMaxTime" ==> "22:00"
+        ]
+        
+
+    ReactBindings.React.createElement(FullCalendarComponent, calendarProps, [])
--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -10,84 +10,61 @@ open type Feliz.Html
 open FullCalendarBindings
 open Fable.Core.JS
 
-type CalendarEvent = {
-    Title: string
-    Start: string
-    End: string
-}
-
 type Model = {
     Events: CalendarEvent list
 }
 
 type Msg =
     | NoOp
+    | CreateEvent of obj
+    | MoveEvent of obj
 
 let init () : Model * Cmd<Msg> =
-    let sampleEvents = [
-        { Title = "Team Meeting"; Start = "2025-05-22"; End = "2025-05-22" }
-        { Title = "Conference"; Start = "2025-05-23"; End = "2025-05-25" }
-    ]
+    let sampleEvents =
+      [ { Id = 3; Title = "Team Meeting"; Start = "2025-05-22"; End = "2025-05-22" }
+        { Id = 4; Title = "Conference"; Start = "2025-05-23"; End = "2025-05-25" }
+      ]
     { Events = sampleEvents }, Cmd.none
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
     | NoOp -> model, Cmd.none
 
-let calendarComponent (events: CalendarEvent list) =
-    let calendarEvents =
-        events
-        |> List.map (fun ev ->
-            createObj [
-                "title" ==> ev.Title
-                "start" ==> ev.Start
-                "end" ==> ev.End
-            ])
-        |> List.toArray
-
-    let calendarProps =
-        createObj [
-            "plugins" ==> [| dayGridPlugin; timeGridPlugin; interactionPlugin |]
-            "initialView" ==> "timeGridWeek"
-            "editable" ==> true
-            "droppable" ==> true
-            "events" ==> [|
-                createObj [ "title" ==> "Meeting"; "start" ==> "2025-05-22" ]
-                createObj [ "title" ==> "Workshop"; "start" ==> "2025-05-23" ]
-            |]
-            // optional callbacks
-            "eventDrop" ==> (fun (info: obj) ->
-                console.log("Event moved:", info)
-            )
-            "drop" ==> (fun (info: obj) ->
-                console.log("External element dropped:", info)
-            )
-            "locale" ==> "de"
-            "locales" ==> [| germanLocale |]
-            "slotMinTime" ==> "06:00"
-            "slotMaxTime" ==> "22:00"
-        ]
-        
-
-    ReactBindings.React.createElement(FullCalendarComponent, calendarProps, [])
 
 
-let CalendarWithDraggable (props: {| model: Model |}) =
+let CalendarWithDraggable (model: Model) =
     React.functionComponent(fun () ->
-
         React.useEffectOnce(fun () ->
             let draggable =
                 createNew Draggable
                     (Browser.Dom.document.getElementById("external-events"),
                         createObj [
                             "itemSelector" ==> "#my-calendar"
-                            "eventData" ==> (fun (_: obj) -> createObj [ "title" ==> "My Calendar" ])
+                            "eventData" ==> (fun (_: obj) -> createObj [ "title" ==> "My Calendar123" ])
                         ])
             None
         )
 
         Html.div [
-            calendarComponent props.model.Events
+            calendarComponent
+                { Events = model.Events
+                  CreateEventF =
+                    (fun info ->
+                        console.log($"CreateEvent", info)
+                    )
+                  MoveEventF =
+                    (fun info ->
+                        console.log($"MoveEvent", info)
+                    )
+                  DeleteEventF =
+                    (fun info ->
+                        console.log($"DeleteEvent", info)
+                    )
+                  EditEventF =
+                    (fun info ->
+                        console.log($"EditEvent", info)
+                    )
+                }
         ]
     )
 
@@ -113,5 +90,5 @@ let view model dispatch =
                 ]
             ]
         ]
-        CalendarWithDraggable {| model = model |} ()
+        CalendarWithDraggable model ()
       ]
