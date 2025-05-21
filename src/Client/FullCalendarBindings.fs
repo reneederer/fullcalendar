660a2c23b443cd6d919f12c236dab112a2168472
reneederer <rene.ederer@protonmail.com>
2025-05-21 18:08:11 +0200

    -

---
+++ src/Client/FullCalendarBindings.fs
@@ -0,0 +1,31 @@
+module FullCalendarBindings
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.React
+
+[<ImportDefault("@fullcalendar/react")>]
+let FullCalendarComponent: obj = jsNative
+
+[<ImportDefault("@fullcalendar/daygrid")>]
+let dayGridPlugin: obj = jsNative
+
+[<ImportDefault("@fullcalendar/interaction")>]
+let interactionPlugin: obj = jsNative
+
+[<Import("Draggable", "@fullcalendar/interaction")>]
+let Draggable: obj = jsNative
+
+[<ImportDefault("@fullcalendar/timegrid")>]
+let timeGridPlugin: obj = jsNative
+
+[<Import("default", "@fullcalendar/core/locales/de")>]
+let germanLocale: obj = jsNative
+
+
+
+type CalendarEvent = {
+    title: string
+    start: string
+    ``end``: string
+}
